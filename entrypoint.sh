aeed109d8d53ea12b779b992f9f1a7a6e2fcd7f6
Patrick Pedrioli <pedrioli@gmail.com>
2017-01-20 09:11:29 +0100

    Allow customization of ids for user inside docker.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Allow ids of user inside the docker to be customized.
+# Either use the LOCAL_USER_ID and LOCAL_GROUP_ID, if passed in at runtime or
+# fallback
+
+USER_ID=${LOCAL_USER_ID:-9001}
+GROUP_ID=${LOCAL_GROUP_ID:-250}
+
+echo usermod -u $USER_ID user
+usermod -u $USER_ID user
+echo groupmod -g $GROUP_ID userg
+groupmod -g $GROUP_ID userg
+
 ## Execute CMD passed by the user when statrting the image
 exec /usr/local/bin/gosu user "$@"
 
