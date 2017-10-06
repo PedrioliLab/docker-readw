1bfc2866796a3d623cf375cf25516ed874c81214
Patrick Pedrioli <p.g.a.pedrioli@dundee.ac.uk>
2017-10-06 10:16:05 +0200

    run conversion using bash

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-docker run -v $PWD:/data -e LOCAL_USER_ID=`id -u $USER` -it --rm ppatrick/readw wine /usr/local/bin/ReAdW.exe $@
+docker run -v $PWD:/data -e LOCAL_USER_ID=`id -u $USER` -it --rm ppatrick/readw bin/bash -c "wine /usr/local/bin/ReAdW.exe $@"
